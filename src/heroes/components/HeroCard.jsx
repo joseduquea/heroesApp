cbacb435677feb135388fad8eb74cdbf7aafb60f
joseduquea <jmduquea23@gmail.com>
2023-06-21 10:24:36 -0500

    assets img change

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -14,7 +14,7 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImageUrl = `/heroes/${ id }.jpg`;
+    const heroImageUrl = `./assets/heroes/${ id }.jpg`;
         //const charactersByHero = (<p>{characters}</p>)
     return (
         <div className="col animate__animated animate__fadeIn">
